96d9c3386e33f36e808ce22491bc0fec969e2815
vanessa fernanda <vanessaprincss.arreo@gmail.com>
2025-08-27 16:40:43 -0600

    :0

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:movil2025/utils/color_app.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int contador = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('uncanivalley'),
-          backgroundColor: Colors.blueGrey,),
+          title: Text('uncanivalley/practica 1'),
+          backgroundColor: Colors.blueGrey,
+        ),
         body: Container(
-          child: Center(child: Text('Hello')),
+          child: Center(
+            child: Text(
+              'Contador: $contador :b',
+              style: TextStyle(fontSize: 25, color: ColorsApp.txtColor),
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.ads_click),
+          onPressed: () {
+            contador++;
+            print(contador);
+            setState(() {
+              
+            });
+          },
         ),
       ),
     );
   }
+
+  miEvnnto() {}
 }
