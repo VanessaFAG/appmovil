80b6ec757a686304107720488e504c4ebb9e3dd6
vanessa fernanda <vanessaprincss.arreo@gmail.com>
2025-09-13 18:35:00 -0600

    hice lo que no tenia que hacer

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -13,6 +13,7 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   TextEditingController conUser = TextEditingController();
+  TextEditingController conEmail = TextEditingController();
   TextEditingController conPass = TextEditingController();
   bool validando = false;
 
@@ -22,20 +23,23 @@ class _LoginScreenState extends State<LoginScreen> {
     final txtUser = TextField(
       keyboardType: TextInputType.emailAddress,
       controller: conUser,
-      decoration: InputDecoration(hintText: 'correo'),
+      decoration: InputDecoration(hintText: 'usuario')
+    );
+    final txtEmail = TextField(
+      keyboardType: TextInputType.emailAddress,
+      controller: conEmail,
+      decoration: InputDecoration(hintText: 'correo')
     );
     final txtPass = TextField(
       obscureText: true,
       controller: conPass,
       decoration:
-      InputDecoration(hintText: 'contaseña'),
-      /*style:
-      TextStyle(color: Color.fromARGB(255, 10, 15,60)),*/
+      InputDecoration(hintText: 'contaseña')
     );
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Container(
-        //visualiza la imagen
+        //visualiza la imagen del fondo
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
@@ -50,7 +54,7 @@ class _LoginScreenState extends State<LoginScreen> {
             Positioned(
               top: 200,
               child: Text(
-                "Gojira",
+                "Bienvenido",
                 style: TextStyle(
                   color: const Color.fromARGB(255, 10, 15, 60),
                   fontSize: 35,
@@ -58,21 +62,42 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
             Positioned(
-              bottom: 75,
+              bottom: 90,
+              width: 320,
               child: Container(
                 padding: EdgeInsets.symmetric(horizontal: 10.0),
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height * .30,
                 decoration: BoxDecoration(
-                  color: Color.fromARGB(86, 185, 185, 185),
+                  color: Color.fromARGB(165, 185, 185, 185),
                   borderRadius: BorderRadius.circular(25),
                 ),
                 child: Column(
                   children: [
+                    SizedBox(height: 10,),
                     txtUser,
+                    txtEmail,
                     txtPass,
                     IconButton(
                       onPressed: () {
+                        if(!validar(conUser.text, conEmail.text, conPass.text)){
+                          showDialog(
+                            context: context, 
+                            builder: (context){
+                              return AlertDialog(
+                                title: Text("Error"),
+                                content: Text("Usuario/Correo o Contraseña incorrectos, por favor vuelva a intentarlo"),
+                                actions: [
+                                  TextButton(
+                                    onPressed: (){
+                                      Navigator.pop(context);
+                                    }, 
+                                    child: Text("Aceptar"))
+                                ],
+                              );
+                            });
+                            return;
+                        }
                         validando = true;
                         setState(() {});
                         Future.delayed(Duration(milliseconds: 3000)).then(
@@ -94,4 +119,12 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
+}
+
+validar(String user, String email, String pass){
+  if(pass.isNotEmpty && (user.isNotEmpty || email.isNotEmpty)){
+//valida que la contraseña no este vacia y que el usuario o correo no esten vacios, el usuario por el momento puede poner uno de los 2
+    return true;
+  }
+  return false;
 }
