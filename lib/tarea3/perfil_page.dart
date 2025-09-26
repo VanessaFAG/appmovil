dd804c6c54b22aced3a4240ad9f8bb70a341af61
vanessa fernanda <vanessaprincss.arreo@gmail.com>
2025-09-25 20:18:51 -0600

    perfil1

--- lib/tarea3/perfil_page.dart
+++ lib/tarea3/perfil_page.dart
@@ -10,20 +10,65 @@ class PerfilPage extends StatefulWidget {
 class _PerfilPageState extends State<PerfilPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        
         children: [
-          SizedBox(
-            child: UserAccountsDrawerHeader(
-              currentAccountPicture: Image(image: NetworkImage("https://thispersondoesnotexist.com/")),
-              accountName: Text("Julia Edwards"),
-              accountEmail: Text("juliaedwards@gmail.com"),
-              //Text("+1234567890"),
+          Container(
+            height: 150,
+            color: Colors.red,
+            child: Center(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage("https://thispersondoesnotexist.com/"), 
+                backgroundColor: Colors.red,
+              ),
             ),
           ),
-          
+
+          const SizedBox(height: 10),
+          Text("Julia Edwards", style: TextStyle(fontSize: 24, color: Colors.black)),
+          Text("juliaedwards@gmail.com", style: TextStyle(fontSize: 20, color: Colors.black)),
+          Text("+1234567890", style: TextStyle(fontSize: 14, color: Colors.grey)),
+
+          const SizedBox(height: 15),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Edit Profile"),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 20),
+          ListTile(
+            leading: Icon(Icons.food_bank, color: Colors.black),
+            title: Text("My Orders", style: TextStyle(fontSize: 18, color: Colors.black)),
+            onTap: () {},
+          ),
+          const SizedBox(height: 20),
+          ListTile(
+            leading: Icon(Icons.location_on, color: Colors.black),
+            title: Text("My Addresses", style: TextStyle(fontSize: 18, color: Colors.black)),
+            onTap: () {},
+          ),
+          const SizedBox(height: 20),
+          ListTile(
+            leading: Icon(Icons.favorite, color: Colors.black),
+            title: Text("My Favourites", style: TextStyle(fontSize: 18, color: Colors.black)),
+            onTap: () {},
+          ),
+          const SizedBox(height: 20),
+          ListTile(
+            leading: Icon(Icons.payment, color: Colors.black),
+            title: Text("Coupons", style: TextStyle(fontSize: 18, color: Colors.black)),
+            onTap: () {},
+          ),
         ],
-      )
+      ),
     );
   }
 }
